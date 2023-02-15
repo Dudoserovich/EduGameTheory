6a107264269cf30787b098d68d77753addc3749a
Dudoserovich <mr.egoryk@mail.ru>
2023-02-16 08:30:05 +1000

    Ref: remove comments

--- server/src/DataFixtures/UserFixtures.php
+++ server/src/DataFixtures/UserFixtures.php
@@ -11,7 +11,6 @@ class UserFixtures extends BaseFixtureAbstract
     public const ROLE_ADMIN = 'ROLE_ADMIN';
     public const ROLE_TEACHER = 'ROLE_TEACHER';
     public const ROLE_USER = 'ROLE_USER';
-//    public const USER_TEAM_COUNT = 10;
     private UserPasswordHasherInterface $passwordHasher;
 
     public function __construct(UserPasswordHasherInterface $passwordHasher)
@@ -21,11 +20,8 @@ class UserFixtures extends BaseFixtureAbstract
     }
 
     /**
-     * Данные генерируются на основе уже существующих специализаций, грейдов и команд:
-     *
      * - Сначала создается администратор. Единственный пользователь с ролью администратора.
-     * - Затем по каждой специализации создаются self::USER_TEAM_COUNT разработчиков.
-     *   Первый всегда имеет роль лида, остальные - нет.
+     * - Затем чередуясь создаются ученики и преподаватели.
      *
      * @param ObjectManager $manager
      * @return void
@@ -43,7 +39,7 @@ class UserFixtures extends BaseFixtureAbstract
         $manager->persist($admin);
         $this->saveReference($admin);
 
-        // Создание лидов и разработчиков.
+        // Создание учеников и преподавателей.
         for ($i = 0; $i < 10; ++$i) {
             $user = new User();
             $user
@@ -55,19 +51,6 @@ class UserFixtures extends BaseFixtureAbstract
             $manager->persist($user);
             $this->saveReference($user);
         }
-
-        // Спизжено с DromUpgrade, но можно изменить, когда создадим фикстуры для всего основного
-//        for ($i = 0; $i < count(SpecFixtures::SPEC_TEAMS) * self::USER_TEAM_COUNT; ++$i) {
-//            $user = new User();
-//            $user
-//                ->setRoles($i % self::USER_TEAM_COUNT ? [self::ROLE_USER] : [self::ROLE_USER, self::ROLE_LEAD])
-//                ->setUsername("login$i")
-//                ->setPassword($this->passwordHasher->hashPassword($user, "password$i"))
-//                ->setFio($this->faker->name)
-//                ->setEmail($this->faker->email);
-//            $manager->persist($user);
-//            $this->saveReference($user);
-//        }
 
         $manager->flush();
     }
