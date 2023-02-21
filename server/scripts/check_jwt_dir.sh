deac23e080e1264cac9b939e35629a9fa5a945ab
Dudoserovich <mr.egoryk@mail.ru>
2023-02-21 12:22:34 +1000

    Fix: invalid flags commands

--- server/scripts/check_jwt_dir.sh
+++ server/scripts/check_jwt_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ! [ -d ./config/jwt/ ]; then
-  php bin/console lexik:jwt:generate-keypair
+  php bin/console lexik:jwt:generate-keypair --no-interaction
 else
-  php bin/console lexik:jwt:generate-keypair --overwrite -n
+  php bin/console lexik:jwt:generate-keypair --overwrite --no-interaction
 fi
