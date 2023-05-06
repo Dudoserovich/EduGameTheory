cc5a1abbb4c41ac107ad25da19f6d1268d55d28b
Dudoserovich <mr.egoryk@mail.ru>
2023-05-06 14:29:30 +1000

    Fix: some fix mercure service

--- client/components/Toast/Toast.js
+++ client/components/Toast/Toast.js
@@ -1,11 +1,11 @@
 import React, {useEffect} from 'react';
-import {toast, ToastContainer} from "react-toastify";
+import {toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Toast = () => {
     useEffect(() => {
-        // let origin = window.location.protocol + '//' + window.location.host + ':8081'
-        let origin = 'https://' + window.location.host
+        let origin = window.location.protocol + '//' + window.location.host
+        //let origin = 'http://' + window.location.host
         // The subscriber subscribes to updates for the https://example.com/users/dunglas topic
         const url = new URL(`${origin}/.well-known/mercure`);
         url.searchParams.append('topic', '/news');
@@ -13,16 +13,40 @@ const Toast = () => {
         const eventSource = new EventSource(url);
         // The callback will be called every time an update is published
         ``
+
+        eventSource.onopen = function() {
+            console.log('Connection SSE opened');
+        };
+
         // eventSource.onmessage = e => console.log(e); // do something with the payload
         eventSource.onmessage = e => {
             let message = JSON.parse(e.data).message
             notify(message)
         }
+
+        eventSource.onerror = function(event) {
+            console.error('Error occurred (SSE):', event);
+
+            if (event.target.readyState === EventSource.CLOSED) {
+                console.log('Connection SSE closed');
+            } else {
+                console.log('Attempting to reconnect SSE...');
+                setTimeout(() => {
+                    eventSource.close();
+
+                    const newEventSource = new EventSource('/news');
+
+                    newEventSource.onopen = eventSource.onopen;
+                    newEventSource.onmessage = eventSource.onmessage;
+                    newEventSource.onerror = eventSource.onerror;
+                }, 5000);
+            }
+        };
     }, [])
 
     return (
         <ToastContainer
-            position="bottom-right"
+            position='bottom-right'
             autoClose={10000}
             hideProgressBar={false}
             newestOnTop={false}
@@ -31,7 +55,7 @@ const Toast = () => {
             pauseOnFocusLoss
             draggable
             pauseOnHover
-            theme="dark"
+            theme='dark'
         />
     );
 }
