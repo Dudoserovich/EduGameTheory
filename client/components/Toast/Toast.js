8178ccf5e599847abe6117876d67282455eb886c
Dudoserovich <mr.egoryk@mail.ru>
2023-05-09 14:01:04 +1000

    Fix: fix get data for subscriber

--- client/components/Toast/Toast.js
+++ client/components/Toast/Toast.js
@@ -5,46 +5,29 @@ import 'react-toastify/dist/ReactToastify.css';
 const Toast = () => {
     useEffect(() => {
         let origin = window.location.protocol + '//' + window.location.host
-        //let origin = 'http://' + window.location.host
-        // The subscriber subscribes to updates for the https://example.com/users/dunglas topic
+
         const url = new URL(`${origin}/.well-known/mercure`);
         url.searchParams.append('topic', '/news');
 
         const eventSource = new EventSource(url);
-        // The callback will be called every time an update is published
-        ``
 
         eventSource.onopen = function() {
             console.log('Connection SSE opened');
         };
 
-        // eventSource.onmessage = e => console.log(e); // do something with the payload
-        eventSource.onmessage = e => {
-            let message = JSON.parse(e.data).message
+        eventSource.addEventListener('news', event => {
+            let message = JSON.parse(event.data).message
+            console.log(event)
             notify(message)
-        }
+        })
 
-        eventSource.onerror = function(event) {
-            console.error('Error occurred (SSE):', event);
+        // eventSource.onmessage = e => console.log(e); // do something with the payload
+        // eventSource.onmessage = e => {
+        //     let message = JSON.parse(e.data).message
+        //     console.log(e)
+        //     notify(message)
+        // }
 
-            if (event.target.readyState === EventSource.CLOSED) {
-                console.log('Connection SSE closed');
-            } else {
-                console.log('Attempting to reconnect SSE...');
-                setTimeout(() => {
-                    eventSource.close();
-
-                    const url = new URL(`${origin}/.well-known/mercure`);
-                    url.searchParams.append('topic', '/news');
-
-                    const newEventSource = new EventSource(url);
-
-                    newEventSource.onopen = eventSource.onopen;
-                    newEventSource.onmessage = eventSource.onmessage;
-                    newEventSource.onerror = eventSource.onerror;
-                }, 5000);
-            }
-        };
     }, [])
 
     return (
