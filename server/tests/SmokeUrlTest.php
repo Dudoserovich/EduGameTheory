d73f08a1a68948eb88fb89b62dde3938fcd40fa4
Dudoserovich <mr.egoryk@mail.ru>
2023-06-14 16:23:03 +1000

    Feat: new smoke tests

--- server/tests/SmokeUrlTest.php
+++ server/tests/SmokeUrlTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Entity\User;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 // TODO: Отсутствуют проверки для путей с указанием `id`
@@ -12,11 +13,18 @@ class SmokeUrlTest extends BaseApiTest
      * @throws TransportExceptionInterface
      */
     public function testPageIsSuccessful(
-        string $method,
-        string $url,
+        string     $method,
+        string     $url,
         array|null $payload = null)
     {
-        if (!$payload)
+        if ($url == '/api/users/{userId}') {
+            $request = self::createClient()->request('GET', '/api/users');
+
+            $user = json_decode($request->getContent())[0];
+            $userId = $user->id;
+
+            self::createClient()->request('GET', "/api/users/$userId");
+        } elseif (!$payload)
             self::createClient()->request($method, $url);
         else
             self::createClient()->request($method, $url, $payload);
@@ -27,21 +35,22 @@ class SmokeUrlTest extends BaseApiTest
     public function urlProvider(): \Generator
     {
         yield ["GET", '/api/users'];
+        yield ["GET", '/api/users/{userId}'];
+
 //        yield ["POST", '/api/users', $this->payload('user_add.json')];
-        yield ["GET", '/api/users/self/avatar'];
-        yield ["GET", '/api/users/avatars'];
-        yield ["GET", '/api/users/self'];
-        yield ["PUT", '/api/users/self', $this->payload('user_change.json')];
-
-        yield ["GET", '/api/terms'];
-//        yield ["POST", '/api/terms', $this->payload('term_add.json')];
-
-        yield ["GET", '/api/literatures'];
-
-        yield ["GET", '/api/achievements'];
-
-        yield ["GET", '/api/topics'];
-
-        yield ["GET", '/api/tasks'];
+//        yield ["GET", '/api/users/avatar/self'];
+//        yield ["GET", '/api/users/self'];
+//        yield ["PUT", '/api/users/self', $this->payload('user_change.json')];
+//
+//        yield ["GET", '/api/terms'];
+////        yield ["POST", '/api/terms', $this->payload('term_add.json')];
+//
+//        yield ["GET", '/api/literatures'];
+//
+//        yield ["GET", '/api/achievements'];
+//
+//        yield ["GET", '/api/topics'];
+//
+//        yield ["GET", '/api/tasks'];
     }
 }
