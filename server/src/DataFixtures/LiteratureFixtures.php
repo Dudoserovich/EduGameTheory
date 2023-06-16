43c799090f26ad414eb045d21d97f6cebb51d7ce
Dudoserovich <mr.egoryk@mail.ru>
2023-06-16 18:08:07 +1000

    Feat: add real fixtures for achievements and terms

--- server/src/DataFixtures/LiteratureFixtures.php
+++ server/src/DataFixtures/LiteratureFixtures.php
@@ -10,6 +10,61 @@ use Doctrine\Persistence\ObjectManager;
 
 class LiteratureFixtures extends BaseFixtureAbstract implements DependentFixtureInterface
 {
+    public const TOPICS = [
+        "Матричные игры",
+        "Кооперативные игры"
+    ];
+    public const LITERATURES = [
+        [
+            "name" => "Онлайн-калькулятор по теории игр",
+            "description" => "Калькулятор по теории игр, содержащий калькуляторы по методам матричных игр, а так же теоретическое описание работы методов",
+            "topic" => [self::TOPICS[0]],
+            "link" => "https://math.semestr.ru/games/games_manual.php"
+        ],
+        [
+            "name" => "PHPSimplex",
+            "description" => "Онлайн-калькулятор для решения матриц Симплекс-методом",
+            "topic" => [self::TOPICS[0]],
+            "link" => "https://www.phpsimplex.com/simplex/simplex.htm?l=en"
+        ],
+        [
+            "name" => "Теория от университета МГСУ",
+            "description" => "Теоретический материал по матричным играм на сайте университета МГСУ",
+            "topic" => [self::TOPICS[0]],
+            "link" => "https://cito.mgsu.ru/COURSES/course753/media/337981978535638/pdf/teoria.pdf"
+        ],
+        [
+            "name" => "Элементы теории кооперативных игр. Университет СО РАН",
+            "description" => "Письменная лекция по кооперативным играм от университета СО РАН (6 апреля 2021г.)",
+            "topic" => [self::TOPICS[1]],
+            "link" => "http://old.math.nsc.ru/~mathecon/Marakulin/CooGAMES.pdf"
+        ],
+        [
+            "name" => "Теория игр. Хабр",
+            "description" => "Статья по теории игр 2012 года на Хабре",
+            "topic" => self::TOPICS,
+            "link" => "https://habr.com/ru/articles/163681/"
+        ],
+        [
+            "name" => "Алексей Савватеев | Теория игр вокруг нас",
+            "description" => "Видеоролик на YouTube. Спикер доступно рассказывает всё о теории игр, применении ее в повседневной жизни и о том, как не проиграть. ",
+            "topic" => self::TOPICS,
+            "link" => "https://www.youtube.com/watch?v=zypuneus6b0"
+        ],
+        [
+            "name" => "Решение матричной игры в чистых стратегиях",
+            "description" => "Видеоролик на YouTube. Спикер (преподаватель университета НОУ ИНТУИТ) доступным языком рассказывает про решение матричных игр в чистых стратегиях.",
+            "topic" => [self::TOPICS[0]],
+            "link" => "https://www.youtube.com/watch?v=4SeUROXfMfY"
+        ],
+        [
+            "name" => "Решение матричной игры в смешанных стратегиях",
+            "description" => "Видеоролик на YouTube. Спикер (преподаватель университета НОУ ИНТУИТ) доступным языком рассказывает про решение матричных игр в смешанных стратегиях.",
+            "topic" => [self::TOPICS[0]],
+            "link" => "https://www.youtube.com/watch?v=5hG8A-455Vk&list=PLDrmKwRSNx7LFZahXMjYKixzfMgi5MqFF&index=5"
+        ]
+    ];
+
     /**
      * @param ObjectManager $manager
      * @return void
@@ -21,27 +76,52 @@ class LiteratureFixtures extends BaseFixtureAbstract implements DependentFixture
          */
         $topics = $this->getReferencesByEntityClass(Topic::class);
 
-        for ($i = 0; $i < 10; ++$i) {
-            $literature = new Literature();
-            $literature
-                ->setName($this->faker->unique()->word())
-                ->setDescription($this->faker->paragraph(1))
-                ->setLink($this->faker->url());
-            $manager->persist($literature);
+//        for ($i = 0; $i < 10; ++$i) {
+//            $literature = new Literature();
+//            $literature
+//                ->setName($this->faker->unique()->word())
+//                ->setDescription($this->faker->paragraph(1))
+//                ->setLink($this->faker->url());
+//            $manager->persist($literature);
+//
+//            # Решение для включения нескольких тем (топиков)
+//            $loopIndex = $this->faker->numberBetween(0, count($topics)-1);
+//            for ($j = 0; $j <= $loopIndex; ++$j) {
+//                $topicLiterature = new TopicLiterature();
+//                $topicLiterature
+//                    ->setLiterature($literature)
+//                    ->setTopic($topics[$j]);
+//
+//                $manager->persist($topicLiterature);
+//                $this->saveReference($topicLiterature);
+//            }
+//
+//            $this->saveReference($literature);
+//        }
 
-            # Решение для включения нескольких тем (топиков)
-            $loopIndex = $this->faker->numberBetween(0, count($topics)-1);
-            for ($j = 0; $j <= $loopIndex; ++$j) {
-                $topicLiterature = new TopicLiterature();
-                $topicLiterature
-                    ->setLiterature($literature)
-                    ->setTopic($topics[$j]);
+        foreach (self::LITERATURES as $literature) {
+            $literatureEntity = new Literature();
+            $literatureEntity
+                ->setName($literature['name'])
+                ->setDescription($literature['description'])
+                ->setLink($literature['link'])
+            ;
+            $manager->persist($literatureEntity);
 
-                $manager->persist($topicLiterature);
-                $this->saveReference($topicLiterature);
+            foreach ($topics as $topicEntity) {
+                if (in_array($topicEntity->getName(), $literature['topic'])) {
+                    $topicLiterature = new TopicLiterature();
+                    $topicLiterature
+                        ->setLiterature($literatureEntity)
+                        ->setTopic($topicEntity)
+                    ;
+
+                    $manager->persist($topicLiterature);
+                    $this->saveReference($topicLiterature);
+                }
             }
 
-            $this->saveReference($literature);
+            $this->saveReference($literatureEntity);
         }
 
         $manager->flush();
--- server/src/DataFixtures/TermFixtures.php
+++ server/src/DataFixtures/TermFixtures.php
@@ -8,6 +8,67 @@ use Doctrine\Persistence\ObjectManager;
 
 class TermFixtures extends BaseFixtureAbstract
 {
+    public const TOPICS = [
+        "Матричные игры",
+        "Кооперативные игры"
+    ];
+    public const TERMS = [
+        [
+            "name" => "Равновесие Нэша",
+            "description" => "Равновесие Нэша — это концепция в теории игр, которая описывает ситуацию, когда игроки выбирают свои стратегии независимо друг от друга и не могут получить больше выигрыша, изменяя свою стратегию в одиночку.",
+            "topic" => self::TOPICS[0]
+        ],
+        [
+            "name" => "Стоимость игры",
+            "description" => "Стоимость игры — это выигрыш или проигрыш, который получает каждый игрок в матричной игре в зависимости от выбранных ими стратегий и стратегий других игроков.",
+            "topic" => self::TOPICS[0]
+        ],
+        [
+            "name" => "Игра с нулевой суммой",
+            "description" => "Игра с нулевой суммой — это матричная игра, в которой выигрыш одного игрока равен проигрышу другого игрока.",
+            "topic" => self::TOPICS[0]
+        ],
+        [
+            "name" => "Игры с коалициями",
+            "description" => "Игры с коалициями — это кооперативные игры, в которых игроки могут объединяться в группы (коалиции), чтобы увеличить свои выигрыши.",
+            "topic" => self::TOPICS[1]
+        ],
+        [
+            "name" => "С-ядро",
+            "description" => "С-ядро — принцип оптимальности в теории кооперативных игр, представляющий собой множество эффективных распределений выигрыша, устойчивых к отклонениям любой коалиции игроков",
+            "topic" => self::TOPICS[1]
+        ],
+        [
+            "name" => "Равновесие по Парето",
+            "description" => "Равновесие по Парето — это состояние, в котором ни один игрок не может улучшить свой выигрыш, не ухудшив выигрыш другого игрока.",
+            "topic" => self::TOPICS[0]
+        ],
+        [
+            "name" => "Система Шепли",
+            "description" => "Система Шепли — это метод распределения выигрышей в кооперативной игре между игроками, который учитывает их вклад в достижение общего результата.",
+            "topic" => self::TOPICS[1]
+        ],
+        [
+            "name" => "Игры с симметричными выигрышами",
+            "description" => "Игры с симметричными выигрышами — это игры, в которых выигрыши каждого игрока совпадают.",
+            "topic" => self::TOPICS[0]
+        ],
+        [
+            "name" => "Матрица выигрышей",
+            "description" => "Матрица выигрышей — это таблица, которая показывает выигрыши или проигрыши каждого игрока в матричной игре в зависимости от выбранных ими стратегий и стратегий других игроков.",
+            "topic" => self::TOPICS[0]
+        ],
+        [
+            "name" => "Коалиция",
+            "description" => "Коалиция — это группа игроков в кооперативной игре, которые объединяются для достижения лучшего результата.",
+            "topic" => self::TOPICS[1]
+        ],
+        [
+            "name" => "Смешанные стратегии",
+            "description" => "Смешанные стратегии — это стратегии, которые включают вероятности выбора каждой из возможных стратегий в матричной игре.",
+            "topic" => self::TOPICS[1]
+        ]
+    ];
 
     /**
      * @param ObjectManager $manager
@@ -16,14 +77,29 @@ class TermFixtures extends BaseFixtureAbstract
     public function load(ObjectManager $manager): void
     {
         $topics = $this->getReferencesByEntityClass(Topic::class);
-        for ($i = 0; $i < 10; ++$i) {
-            $term = new Term();
-            $term
-                ->setName($this->faker->unique()->word())
-                ->setDescription($this->faker->paragraph(2))
-                ->setTopic($this->faker->randomElement($topics));
-            $manager->persist($term);
-            $this->saveReference($term);
+//        for ($i = 0; $i < 10; ++$i) {
+//            $term = new Term();
+//            $term
+//                ->setName($this->faker->unique()->word())
+//                ->setDescription($this->faker->paragraph(2))
+//                ->setTopic($this->faker->randomElement($topics));
+//            $manager->persist($term);
+//            $this->saveReference($term);
+//        }
+
+
+        foreach (self::TERMS as $term) {
+            foreach ($topics as $topic) {
+                if ($topic->getName() === $term['topic']) {
+                    $termEntity = new Term();
+                    $termEntity
+                        ->setName($term['name'])
+                        ->setDescription($term['description'])
+                        ->setTopic($topic);
+                    $manager->persist($termEntity);
+                    $this->saveReference($termEntity);
+                }
+            }
         }
 
         $manager->flush();
