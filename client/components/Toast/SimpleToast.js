0636fb8b49ff780bacae4245e501be3c943fd351
Dudoserovich <mr.egoryk@mail.ru>
2023-07-03 23:35:55 +1000

    Feat: add new toasts

--- client/components/Toast/SimpleToast.js
+++ client/components/Toast/SimpleToast.js
@@ -3,7 +3,7 @@ import {toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {useDispatch, useSelector} from "react-redux";
 
-const ToastSSE = () => {
+const SimpleToast = () => {
 
     return (
         <ToastContainer
@@ -16,10 +16,14 @@ const ToastSSE = () => {
             pauseOnFocusLoss
             draggable
             pauseOnHover
+            limit={5}
             theme='dark'
         />
     );
 }
 
-export default ToastSSE;
+export default SimpleToast;
 export const notify = (message) => toast(message);
+export const notifyWarn = (message) => toast.warn(message);
+export const notifyError = (message) => toast.error(message);
+export const notifySuccess = (message) => toast.success(message);
