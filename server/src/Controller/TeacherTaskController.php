1a1f9c470b09dd67ad1c5ca9f32b34997e523dd8
Dudoserovich <mr.egoryk@mail.ru>
2023-07-05 03:41:31 +1000

    Feat: add info about matrix

--- server/src/Controller/TeacherTaskController.php
+++ server/src/Controller/TeacherTaskController.php
@@ -130,7 +130,7 @@ class TeacherTaskController extends ApiController
     }
 
     /**
-     * Получение результата по конкретному заданию
+     * Получение результата по конкретному своему заданию
      * @OA\Response(
      *     response=200,
      *     description="HTTP_OK"
@@ -151,14 +151,14 @@ class TeacherTaskController extends ApiController
         methods: ['GET']
     )]
     public function getSelfTask(
-        int $taskId,
+        int               $taskId,
         TaskMarkPreviewer $taskMarkPreviewer
     ): JsonResponse
     {
         $user = $this->getUserEntity($this->userRepository);
         $task = $this->taskRepository->findOneBy(["owner" => $user, "id" => $taskId]);
         if (!$task) {
-            return $this->respondNotFound("Задание не найдена");
+            return $this->respondNotFound("Задание не найдено");
         }
 
         $taskMarksBySelfTask = $this->taskMarkRepository->findBy(["task" => $task]);
@@ -169,6 +169,91 @@ class TeacherTaskController extends ApiController
         );
 
         return $this->response($result);
+    }
+
+    /**
+     * Получение информации о матрице по двумерному массиву и флагу матрицы
+     * @OA\RequestBody (
+     *     required=true,
+     *     description="Получение информации о заданной матрице",
+     *     @OA\JsonContent(
+     *         example={
+     *              "matrix": {{0, 1, 1}, {1, 0, 1}, {1, 1, 0}},
+     *              "flag_matrix": "платёжная матрица",
+     *         },
+     *         @OA\Property(property="matrix", ref="#/components/schemas/TaskView/properties/matrix"),
+     *         @OA\Property(property="flag_matrix", ref="#/components/schemas/TaskView/properties/flag_matrix"),
+     *     )
+     * )
+     * @OA\Response(
+     *     response=200,
+     *     description="HTTP_OK"
+     * )
+     * @OA\Response(
+     *     response=403,
+     *     description="Доступ запрещен"
+     * )
+     * @OA\Response(
+     *     response=404,
+     *     description="Не найдено"
+     * )
+     * @OA\Response(
+     *     response=422,
+     *     description="Некорректные данные"
+     * )
+     */
+    #[Route('/matrixInfo',
+        name: 'get_info_about_matrix',
+        methods: ['PUT']
+    )]
+    public function solveTask(
+        Request $request
+    ): JsonResponse
+    {
+        $request = $request->request->all();
+        $this->setSoftDeleteable($this->em, false);
+
+        if (!$request['matrix']) {
+            return $this->respondValidationError('Задана пустая матрица');
+        }
+        // Матрица последствий
+        if ($request['flag_matrix'] != 'платёжная матрица') {
+            try {
+                $solve = TaskSolver::solveRiskMatrix($request['matrix']);
+            } catch (BadDataException|IncorrectTypeException
+            |MatrixException|MathException|Exception $e) {
+                return $this->respondValidationError($e->getMessage());
+            }
+        } else {
+            // Платёжная матрица
+            try {
+                $solve = TaskSolver::solvePayoffMatrix($request['matrix']);
+            } catch (BadDataException|IncorrectTypeException
+            |MatrixException|MathException|Exception $e) {
+                return $this->respondValidationError($e->getMessage());
+            }
+        }
+
+        $strategy = '';
+        $method = '';
+        if (!isset($solve['strategy'])) {
+            $method = "Лапласа";
+
+            $message = "Создаваемое задание можно будет решить методом **\"$method\"**. ";
+
+        } else {
+            if ($solve['strategy'] === 'смешанные стратегии') {
+                $strategy = "смешанных стратегиях";
+                $method = "Минимакс";
+            } elseif ($solve['strategy'] === 'чистые стратегии') {
+                $strategy = "чистых стратегиях";
+                $method = "Симплекс-метод";
+            }
+            $message = "Задание задано в **$strategy**. 
+                    Решить задание можно с помощью " . "**\"$method" . "а" . '"**. ';
+        }
+
+        return $this->response(array("message" => $message));
     }
 
 }
